96ed68b959a390273d1509769a183970cde34b1e
AlyNa <alyna.khairani@gmail.com>
2020-11-03 14:53:53 +0500

    Update SecondLargest.java

--- PS05/SecondLargest.java
+++ PS05/SecondLargest.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/* Given the size of an array of doubles as N, get all numbers from the user. Afterward, find and output the second largest element from the array */
+
 public class SecondLargest {
     public static void main(String[] args) {
         Scanner input2 = new Scanner(System.in);
